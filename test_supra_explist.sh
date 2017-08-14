d6f669eea466275ca026608c7308184c0fdf87ec
Francisco Paz-Chinchon <fpazchin@des51.fnal.gov>
2017-08-13 23:22:44 -0500

    change path

--- test_supra_explist.sh
+++ test_supra_explist.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python supra_explist.py --nite 20170201 --d_msk /data/des51.a/data/fpazchin/Code/diff_imaging/des-diffimg-small --user fpazch --test
+python supra_explist.py --nite 20170201 --user fpazch --test
