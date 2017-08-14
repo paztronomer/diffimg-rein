26629f6ff03c2efb955c1334bef44df0ba0dd691
Francisco Paz-Chinchon <fpazchin@des51.fnal.gov>
2017-08-13 21:47:23 -0500

    removed unnecesary setup

--- setup.sh
+++
@@ -1,8 +0,0 @@
-
-source /cvmfs/des.opensciencegrid.org/eeups/startupcachejob21i.sh
-#source /cvmfs/des.opensciencegrid.org/eeups/startupcachejob.sh
-#setup jobsub_client
-kx509
-voms-proxy-init -rfc -noregen -voms des:/des/Role=DESGW
-
-
